2a695c064ce828707e4b00ec817a5ee83220047e
ProgrammerDSP <86547056+ProgrammerDSP@users.noreply.github.com>
2021-07-27 22:08:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -9,60 +9,42 @@ all_dirs = []
 paths = {}
 
 def movefiles(path):
-
     try:
         os.chdir(path)
         os.mkdir("Images")
-        os.mkdir("Files")
-        os.mkdir("Documents")
         os.mkdir("Videos")
+        os.mkdir("Documents")
         os.mkdir("Audio")
-    
+        os.mkdir("Files")
+
     except Exception as e:
         pass
 
     for f in all_files:
-
         try:
+            if ".png" in f or ".jpg" in f or ".jpeg" in f:
+                filespath = paths[f]
+                path = os.path.join(os.getcwd() , "Images")
+                shutil.move(filespath , path)
 
-            if ".png" in f or ".jpg" in f or ".jpeg" in f or ".jfif" in f:
-                filepath = paths[f]
-                path = os.path.join(os.getcwd(), "Images")
-                shutil.move(filepath, path)
+            elif ".py" in f or ".js" in f:
+                filespath = paths[f]
+                path = os.path.join(os.getcwd() , "Files")
+                shutil.move(filespath , path)
 
-            elif ".py" in f or ".js" in f or ".html" in f or ".txt" in f or ".css" in f:
-                filepath = paths[f]
-                path = os.path.join(os.getcwd(), "Files")
-                shutil.move(filepath, path)
-
-            elif ".xlsx" in f or ".doc" in f or ".xlx" in f or ".pdf" in f:
-                filepath = paths[f]
-                path = os.path.join(os.getcwd(), "Documents")
-                shutil.move(filepath, path)
-
-            elif ".mp4" in f or ".mkv" in f or ".gif" in f or ".webm" in f or ".flv" in f:
-                filepath = paths[f]
-                path = os.path.join(os.getcwd(), "Videos")
-                shutil.move(filepath, path)
-
-            elif ".mp3" in f:
-                filepath = paths[f]
-                path = os.path.join(os.getcwd(), "Audio")
-                shutil.move(filepath, path)
-
-        except Exception as e:
+        except:
             pass
 
 while True:
-    for root, directories, files in os.walk(path, topdown=False):
-        for name in files:
-            all_files.append(name)
-            pathF = os.path.join(root, name)
-            paths[name] = pathF
+    for path , folders , files in os.walk(path, topdown=False):
+        for n in files:
+            all_files.append(n)
+            pathF = os.path.join(path , n)
+            paths[n] = pathF
 
-        for name in directories:
-            all_dirs.append(name)
-            pathF = os.path.join(root, name)
-            paths[name] = pathF
+        for n in folders:
+            all_dirs.append(n)
+            pathF = os.path.join(path , n)
+            paths[n] = pathF
 
     movefiles(path)
